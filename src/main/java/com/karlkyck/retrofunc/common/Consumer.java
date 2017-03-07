824a42fef3d889186c392eff5046839dca3b9372
Karl Kyck <karlkyck@gmail.com>
2017-03-07 09:58:16 +0000

    Added ListUtilities for immutable operations on List objects.

---
+++ src/main/java/com/karlkyck/retrofunc/common/Consumer.java
@@ -0,0 +1,9 @@
+package com.karlkyck.retrofunc.common;
+
+/**
+ * Copyright WithWoof 2017
+ * Created by karlkyck on 07/03/17.
+ */
+public interface Consumer<T> {
+	void accept(T t);
+}
---
+++ src/main/java/com/karlkyck/retrofunc/common/ListUtilities.java
@@ -0,0 +1,21 @@
+package com.karlkyck.retrofunc.common;
+
+import java.util.List;
+
+/**
+ * Copyright WithWoof 2017
+ * Created by karlkyck on 07/03/17.
+ */
+public class ListUtilities {
+	/**
+	 * Applies f to each item in the supplied list
+	 *
+	 * @param list   List to iterate over
+	 * @param action Consumer to against which each element in the supplied List is applied
+	 * @param <T>    Type of elements within the List
+	 */
+	public static <T, R> void forEach(List<T> list, Consumer<T> action) {
+		for (T t : list)
+			action.accept(t);
+	}
+}
